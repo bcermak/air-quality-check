2853bd71c166faca857efc891bcf1e846b21d929
Brandon Cermak <cermakprofessional@gmail.com>
2023-08-15 16:01:08 -0500

    set up initial API calls/env file to hide KEY

--- air-app/server.js
+++ air-app/server.js
@@ -1,5 +1,28 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+var axios = require('axios');
+var FormData = require('form-data');
+var data = new FormData();
+
+var config = {
+  method: 'get',
+maxBodyLength: Infinity,
+  url: `http://api.airvisual.com/v2/station?station={{STATION_NAME}}&city={{CITY_NAME}}&state={{STATE_NAME}}&country={{COUNTRY_NAME}}&key=${process.env.API_KEY}`,
+  headers: { 
+    ...data.getHeaders()
+  },
+  data : data
+};
+
+axios(config)
+.then(function (response) {
+  console.log(JSON.stringify(response.data));
+})
+.catch(function (error) {
+  console.log(error);
+});
+
 
 const port = process.env.PORT || 8080
 app.listen(port, ()=>console.log(`Listening on port ${port}`));
